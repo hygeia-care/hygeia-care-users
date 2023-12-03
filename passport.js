edd0488bb5e165465db985d542d1073d127b1b7e
davrodgar <davrodgar2@alum.us.es>
2023-12-03 14:54:23 +0100

    Authentication using API Keys

---
+++ passport.js
@@ -0,0 +1,24 @@
+const passport = require("passport");
+const Bearer = require('passport-http-bearer').Strategy;
+const ApiKey = require('./models/apikey');
+
+
+
+
+passport.use(new Bearer (
+    async (token, cb) => {
+        try {
+            const user = await ApiKey.findOne({ apikey: token });
+
+            if (!user) {
+                return cb(null, false, { message: 'Unknown apikey ' + token });
+            }
+
+            return cb(null, user);
+        } catch (err) {
+            return cb(err);
+        }
+    }
+));
+
+module.exports = passport;
