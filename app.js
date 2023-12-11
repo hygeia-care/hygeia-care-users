8d99fe5349da6e4f7360af452e9b7736c7c0859c
javvazzam <javvazzam@alum.us.es>
2023-12-11 16:45:56 +0100

    Adicion de CORS para realizar peticiones desde front

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+const cors = require('cors');
+
 const passport = require('./passport');
 
 var indexRouter = require('./routes/index');
@@ -17,6 +19,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(passport.initialize());
+app.use(cors())
 
 require('dotenv').config();
 
