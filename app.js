7f883e1df27d2c6d08b1a1dab5efd867a6073b01
davrodgar <davrodgar2@alum.us.es>
2024-01-02 20:40:32 +0100

    Configurar CORS

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const cors = require('cors'); // importamos CORS
+
 
 const passport = require('./passport');
 
@@ -17,6 +19,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(passport.initialize());
+
+app.use(cors()); // Aplicamos CORS a todas las rutas
 
 require('dotenv').config();
 
