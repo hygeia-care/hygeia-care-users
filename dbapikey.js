9cd27bd8166b3e5481cf05ece4a4de4652fea0ab
davrodgar <davrodgar2@alum.us.es>
2024-01-10 17:12:00 +0100

    Configuración de puerto al 3333 definitivo Organización de claves

--- dbapikey.js
+++ dbapikey.js
@@ -1,7 +1,14 @@
+require('dotenv').config();
 var mongoose = require("mongoose");
 
-const dbapikey = mongoose.createConnection('mongodb+srv://apikey_SRV:QPeebigVw4YHOOnrZ3SG@cluster0.miuwv1w.mongodb.net/apikeys');
-console.log("dbapikey.js / Connecting to database: %s", 'mongodb+srv://apikey_SRV:QPeebigVw4YHOOnrZ3SG@cluster0.miuwv1w.mongodb.net/apikeys');
+
+//const DB_URL = (process.env.DB_URL || 'mongodb://localhost')
+//const DB_URL = "mongodb+srv://assurance:kUnHHe6RI5dWS2VZ@cluster0.miuwv1w.mongodb.net/bills" 
+//const dbapikey = mongoose.createConnection('mongodb+srv://apikey_SRV:QPeebigVw4YHOOnrZ3SG@cluster0.miuwv1w.mongodb.net/apikeys');
+
+const dbApiKeyConnectionString = process.env.DB_API_KEY_CONNECTION_STRING;
+const dbapikey = mongoose.createConnection(dbApiKeyConnectionString);
+console.log("dbapikey.js / Connecting to database: %s", process.env.DB_API_KEY_CONNECTION_STRING);
 dbapikey.on('error', console.error.bind(console, 'dbapikey connection error'));
 
 module.exports = dbapikey
--- dbuser.js
+++ dbuser.js
@@ -1,9 +1,17 @@
+require('dotenv').config();
 var mongoose = require("mongoose");
 
 //'mongodb+srv://users_SRV:QPeebigVw4YHOOnrZ3SG@cluster0.miuwv1w.mongodb.net/users'
 //'mongodb://mongoadmin:secret@localhost:27017/'
-const dbuser = mongoose.createConnection('mongodb+srv://users_SRV:QPeebigVw4YHOOnrZ3SG@cluster0.miuwv1w.mongodb.net/users');
-console.log("dbuser.js / Connecting to database: %s", 'mongodb+srv://users_SRV:QPeebigVw4YHOOnrZ3SG@cluster0.miuwv1w.mongodb.net/users');
+
+//const dbuser = mongoose.createConnection('mongodb+srv://users_SRV:QPeebigVw4YHOOnrZ3SG@cluster0.miuwv1w.mongodb.net/users');
+//console.log("dbuser.js / Connecting to database: %s", 'mongodb+srv://users_SRV:QPeebigVw4YHOOnrZ3SG@cluster0.miuwv1w.mongodb.net/users');
+//dbuser.on('error', console.error.bind(console, 'dbuser connection error'));
+
+const dbUserConnectionString = process.env.DB_USER_CONNECTION_STRING;
+const dbuser = mongoose.createConnection(dbUserConnectionString);
+console.log("dbuser.js / Connecting to database: %s", process.env.DB_USER_CONNECTION_STRING);
 dbuser.on('error', console.error.bind(console, 'dbuser connection error'));
+
 
 module.exports = dbuser
